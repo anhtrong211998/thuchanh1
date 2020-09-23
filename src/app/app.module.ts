6c0a076db4f746b99726076beb267c1f0f2af584
anhtrong211998 <anhtrong211998@gmail.com>
2020-09-23 14:30:17 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Bai1Component } from './bai1/bai1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Bai1Component
   ],
   imports: [
     BrowserModule,
